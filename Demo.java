7397b1d068bc6f5b10c1c91297a6c8c2707ee95b
Harsh-bugs4ever <harshsinghathena@gmail.com>
2025-10-11 22:35:48 +0530

    equal to not equal to

---
+++ Demo.java
@@ -0,0 +1,15 @@
+ class Demo{
+     public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+        boolean result1 =x<y; //using boolean to check whether the given condition is T or F
+        System.out.println(result1);
+        boolean result2 = x!=y; //not equal to
+        // same for equal to x==y;
+        System.out.println(result2);
+        double a = 5.5;
+        int b = 5;
+        boolean result = a<=b;
+        System.out.println(result);
+    }
+}
