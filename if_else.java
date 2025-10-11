f46ed5ddb2222b43f45b695c0e7bd5ffdfdeb46a
Harsh-bugs4ever <harshsinghathena@gmail.com>
2025-10-11 23:34:39 +0530

    more codes

---
+++ if_else.java
@@ -0,0 +1,20 @@
+public class if_else {
+    public static void main(String[] args) {
+      
+      /*  int x = 8;
+       if (x>10) // we can use to print if staement and false to print else statement
+           System.out.println("hello");
+       else
+           System.out.println("bye"); */
+        //2 using logical operators in if else
+        int x = 6;
+        if (x>10 && x<15)
+        {
+        System.out.println("kaka bagh"); // you have to use {} in case if you want to print multiple statements
+        System.out.println("jevlas ka");
+        }
+        else
+        System.out.println("jhale aata");
+        
+    }
+}
--- logical_operators.java
+++ logical_operators.java
@@ -4,11 +4,11 @@ public class logical_operators {
        int y = 5;
        int a = 5;
        int b = 9;
-       boolean result = x > y && a>b;
+       boolean result = x > y && a>b; // and operator
        System.out.println(result); 
-       boolean result2 = x>y || a>b;
+       boolean result2 = x>y || a>b; // OR operator
        System.out.println(result2);
        boolean result3 = x>y ;
-       System.out.println(!result3);
+       System.out.println(!result3); //not operator
     }
 }
