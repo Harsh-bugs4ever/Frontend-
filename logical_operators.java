c182d57938849cc109eb53ddbb1329701993e932
Harsh-bugs4ever <harshsinghathena@gmail.com>
2025-10-11 22:52:41 +0530

    logical opertor

---
+++ logical_operators.java
@@ -0,0 +1,14 @@
+public class logical_operators {
+    public static void main(String[] args) {
+       int x = 7;
+       int y = 5;
+       int a = 5;
+       int b = 9;
+       boolean result = x > y && a>b;
+       System.out.println(result); 
+       boolean result2 = x>y || a>b;
+       System.out.println(result2);
+       boolean result3 = x>y ;
+       System.out.println(!result3);
+    }
+}
